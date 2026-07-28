f0bbc1b813e4286270d7237e26d72b9694628816
d1niss <dinismbraposo3@gmail.com>
2026-07-28 18:22:05 +0100

    remove unecessary imports

--- src/main/java/spotifyvs/SpotifyImporter.java
+++ src/main/java/spotifyvs/SpotifyImporter.java
@@ -16,15 +16,9 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
 import se.michaelthelin.spotify.SpotifyApi;
-import se.michaelthelin.spotify.model_objects.specification.Paging;
-import se.michaelthelin.spotify.model_objects.specification.PlaylistTrack;
-import se.michaelthelin.spotify.model_objects.specification.Track;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+
 import com.google.gson.JsonParser;
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
